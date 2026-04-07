e4849bf5cf32314c5d3393e9a929e4f2d52deae2
Hasrat270 <hasrat3701@gmail.com>
2026-04-07 17:35:31 +0500

    practiced factorial

--- factorial.py
+++ factorial.py
@@ -1,4 +1,5 @@
 num = int(input("Enter any number: "))
+# int typecasting yani type conversion ke liye lagaya he kyu ke input as a string consider horaha tha 
 
 def recursive_factorial(num):
     if num == 0 or num == 1:
@@ -6,3 +7,4 @@ def recursive_factorial(num):
     return num * recursive_factorial(num - 1)
 
 print(f"Factorial of {num} is: ",recursive_factorial(num))
+# string literals mene js me seekhi thi aur python me research ki to isme bhi syntax milgaya 🤣
