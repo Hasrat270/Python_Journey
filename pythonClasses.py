0c91818f8ad8f4aedc208d17199b17d6c74f6dbe
Hasrat270 <hasrat3701@gmail.com>
2026-04-07 19:05:31 +0500

    Python Classes/Objects

---
+++ pythonClasses.py
@@ -0,0 +1,11 @@
+# Python Classes/Objects Code Challenge
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Hasrat", 21)
+p1.greet()
