6ed06a7ee0f163bad49e4b79edd9efcd786284bc
Hasrat270 <hasrat3701@gmail.com>
2026-04-07 21:10:37 +0500

    polymorphismIntro

--- polymorphismIntro.py
+++ polymorphismIntro.py
@@ -40,11 +40,23 @@ class Plane:
   def by(self):
    print("by Air")
 
-car1 = Car("Vigo", "2016")       #Create a Car object
-boat1 = Boat("Kashti", "1965")               #Create a Boat object
-plane1 = Plane("Aeroplane", "C-130")     #Create a Plane object
+car1 = Car("Vigo", "2016")       #Car object
+boat1 = Boat("Kashti", "1965")               #Kashti object
+plane1 = Plane("Aeroplane", "C-130")     #Jahaz object
 
 for x in (car1, boat1, plane1):
   x.move()
   x.printBrandModel()
   x.by()
+
+# car1.move()
+# car1.printBrandModel()
+# car1.by()
+
+# boat1.move()
+# boat1.printBrandModel()
+# boat1.by()
+
+# plane1.move()
+# plane1.printBrandModel()
+# plane1.by()
